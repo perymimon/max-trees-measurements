72efc73040b7de0bcc7004586ed2682d81bcc6b1
perym <pery.mimon@gmail.com>
2022-03-11 14:24:34 +0200

    fix performance issue

--- src/components/tree-fiver/MiniMap.jsx
+++ src/components/tree-fiver/MiniMap.jsx
@@ -1,6 +1,5 @@
-import {useRef} from 'react'
-import {useFrame} from '@react-three/fiber'
-import {useScroll,  Html} from '@react-three/drei'
+import {useEffect, useRef} from 'react'
+import {useScroll, Html} from '@react-three/drei'
 
 import './MiniMap.css'
 
@@ -9,37 +8,41 @@ let pageIndex = -1;
 export function Minimap({items, onPageChange = null}) {
     const ref = useRef()
     const scroll = useScroll()
-    // console.log('scroll', scroll);
+    console.log('scroll', scroll);
 
-    useFrame((state, delta) => {
-        // fire event when page changes
-        const page = Math.floor(scroll.offset * scroll.pages)
-        if(page !== pageIndex) {
-            pageIndex = page
-            if(onPageChange) {
-                onPageChange(page)
+    useEffect(() => {
+        scroll.el.addEventListener('scroll', () => {
+            // fire event when page changes
+            const page = Math.floor(scroll.offset * scroll.pages)
+            if (page !== pageIndex) {
+                pageIndex = page
+                if (onPageChange) {
+                    onPageChange(page)
+                }
             }
-        }
-        // update the scaleY of each Line
-        for (let child of ref.current.children) {
-            let index = +child.dataset.index
-            // Give me a value between 0 and 1
-            //   starting at the position of my item
-            // in the edges it will be 0 but with marging of x * units
-            var unit = 1 / items.length;
-            var center = index * unit
-            var margin = 1.5 * unit
-            const y = scroll.curve(center,  unit,  margin )
-            child.style.setProperty('--scaleY', 0.5 + y)
-        }
-    })
+            // update the scaleY of each Line
+            for (let child of ref.current.children) {
+                let index = +child.dataset.index
+                // Give me a value between 0 and 1
+                //   starting at the position of my item
+                // in the edges it will be 0 but with marging of x * units
+                var unit = 1 / items.length;
+                var center = index * unit
+                var margin = 1.5 * unit
+                const y = scroll.curve(center, unit, margin)
+                child.style.setProperty('--scaleY', 0.5 + y)
+            }
+        })
+
+    }, [])
 
     return (
         <Html ref={ref}
               className="minimap"
-              calculatePosition={(el, camera, size)=>[size.width/2, 50]} >
+              calculatePosition={(el, camera, size) => [size.width / 2, 50]}>
             {items.map((dayText, i) => {
-                return <div key={i} data-index={i} className="line" >
+                return <div key={i}
+                            data-index={i} className="line">
                     {dayText}
                 </div>
             })}
